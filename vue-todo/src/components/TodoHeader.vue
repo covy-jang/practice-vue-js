3ae48b468ca9cad6986ab3f78a94c5b7ae119a7d
stvens@naver.com <stvens@naver.com>
2022-08-22 18:20:39 +0900

    vuex initial commit

--- vue-todo/src/components/TodoHeader.vue
+++ vue-todo/src/components/TodoHeader.vue
@@ -1,31 +1,13 @@
 <template>
-    <div>
-        <h1>TODO it!</h1>
-    </div>
+  <div>
+    <h1>TODO it!</h1>
+  </div>
 </template>
-
-<script>
-export default {
-    data() {
-        return {
-            
-        };
-    },
-
-    mounted() {
-        
-    },
-
-    methods: {
-        
-    },
-};
-</script>
 
 <style scoped>
 h1 {
-    color: #2F3B52;
-    font-weight: 900;
-    margin: 2.5rem 0 1.5rem;
+  color: #2f3b52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
 }
 </style>
